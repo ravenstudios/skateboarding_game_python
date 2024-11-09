4d06ebf8dd3166a19fa5e60dccf0081816967222
ravenstudios <ravenstudios@ravenstudioss-MacBook-Pro.local>
2024-11-09 10:22:30 -0600

    init

---
+++ main.py
@@ -0,0 +1,48 @@
+from constants import *
+import pygame
+
+
+
+clock = pygame.time.Clock()
+surface = pygame.display.set_mode((GAME_WIDTH, GAME_HEIGHT))
+
+pygame.init()
+
+
+def main():
+    running = True
+
+    while running:
+        clock.tick(TICK_RATE)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            if event.type == pygame.KEYDOWN:
+                keys = pygame.key.get_pressed()
+                if event.key == pygame.K_r:
+                    board.reset()
+                if event.key == pygame.K_q:
+                    running = False
+        draw()
+        update()
+
+    pygame.quit()
+
+
+
+def draw():
+    surface.fill((0, 0, 0))#background
+
+    pygame.draw.rect(surface, (255, 255, 255), (50, 50, 50, 50))
+    pygame.display.flip()
+
+
+
+def update():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
